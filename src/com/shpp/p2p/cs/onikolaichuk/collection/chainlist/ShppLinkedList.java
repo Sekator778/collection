d566242f3809e3e1ddc31bb5c5244c4de1557021
Sekator778 <sekator778@gmail.com>
2022-07-01 17:13:13 +0300

    linkedList comment

--- src/com/shpp/p2p/cs/onikolaichuk/collection/chainlist/ShppLinkedList.java
+++ src/com/shpp/p2p/cs/onikolaichuk/collection/chainlist/ShppLinkedList.java
@@ -2,7 +2,14 @@ package com.shpp.p2p.cs.onikolaichuk.collection.chainlist;
 
 import java.util.*;
 
+/**
+ * this data structure is based on nodes that contain data
+ * and a link to the next node
+ *
+ * @param <E> generic
+ */
 public class ShppLinkedList<E> implements Iterable<E> {
+    /* start node in the chain */
     private Node<E> head;
     private int size;
     private int modCount;
@@ -10,9 +17,9 @@ public class ShppLinkedList<E> implements Iterable<E> {
     /**
      * Adds the specified element to the end of this list.
      */
-    public boolean add(E element) {
+    public boolean add(E data) {
         if (head == null) {
-            head = new Node<>(element);
+            head = new Node<>(data);
             size++;
             modCount++;
         } else {
@@ -20,22 +27,24 @@ public class ShppLinkedList<E> implements Iterable<E> {
             while (temp.getNext() != null) {
                 temp = temp.getNext();
             }
-            temp.setNext(new Node<>(element));
+            temp.setNext(new Node<>(data));
             size++;
             modCount++;
         }
         return true;
     }
 
-
     /**
      * Inserts an element at the specified position in this list.
      * Throws IndexOutOfBoundsException,
      * if the specified index is out of range (index < 0 || index > size()).
      */
-    public void add(int index, E element) {
+    public void add(int index, E data) {
+        if (isIndexNoCorrect(index)) {
+            throw new IndexOutOfBoundsException("wrong index " + index);
+        }
         if (head == null || index == 0) {
-            Node<E> nodeAdd = new Node<>(element);
+            Node<E> nodeAdd = new Node<>(data);
             nodeAdd.setNext(head);
             head = nodeAdd;
             size++;
@@ -45,11 +54,43 @@ public class ShppLinkedList<E> implements Iterable<E> {
             int i = 0;
             while (current.getNext() != null) {
                 if (i == index) {
-                    linkBefore(current, element);
+                    linkBefore(current, data);
                     break;
                 }
                 current = current.getNext();
                 i++;
+            }
+        }
+    }
+
+    public void add2(int index, E data) {
+        if (isIndexNoCorrect(index)) {
+            throw new IndexOutOfBoundsException("wrong index " + index);
+        }
+        if (head == null || index == 0) {
+            Node<E> nodeAdd = new Node<>(data);
+            nodeAdd.setNext(head);
+            head = nodeAdd;
+            size++;
+            modCount++;
+        } else {
+            Node<E> current = head;
+            Node<E> next = current.next;
+            int k = 0;
+            while (current != null) {
+                if (k == index) {
+                    Node<E> tmp = current;
+                    current = new Node<>(data);
+                    current.next = tmp;
+                    tmp.next = next;
+                    size++;
+                    modCount++;
+                    break;
+                } else {
+                    current = current.next;
+                    next = current.next;
+                    k++;
+                }
             }
         }
     }
@@ -78,25 +119,30 @@ public class ShppLinkedList<E> implements Iterable<E> {
         return true;
     }
 
-    public void addFirst(E element) {
+    /**
+     * insert new element in the head of the chain
+     *
+     * @param data - element for add to node
+     */
+    public void addFirst(E data) {
         if (head == null) {
-            this.head = new Node<>(element);
+            this.head = new Node<>(data);
             size++;
             modCount++;
         } else {
-            add(0, element);
+            add(0, data);
         }
     }
 
-    public void addLast(E element) {
-        linkLast(element);
+    public void addLast(E data) {
+        linkLast(data);
     }
 
     /**
      * returns head but does not delete
      * if list empty return null
      *
-     * @return first element data
+     * @return first data
      */
     public E peek() {
         return head == null ? null : head.data;
@@ -105,7 +151,7 @@ public class ShppLinkedList<E> implements Iterable<E> {
     /**
      * returns and remove head
      *
-     * @return element head
+     * @return data head
      */
     public E poll() {
         if (head == null) {
@@ -119,43 +165,48 @@ public class ShppLinkedList<E> implements Iterable<E> {
     }
 
     /**
-     * insert a new node from the element before the given one
+     * insert a new node from the data before the given one
      */
-    private void linkBefore(Node<E> node, E element) {
-        Node<E> pred = null;
-        pred = geteNode(node, pred);
-        Node<E> newNode = new Node<>(element);
-        pred.setNext(newNode);
+    private void linkBefore(Node<E> node, E data) {
+        Node<E> previous = null;
+        previous = geteNode(node, previous);
+        Node<E> newNode = new Node<>(data);
+        previous.setNext(newNode);
         newNode.setNext(node);
         size++;
         modCount++;
     }
 
-    private void linkLast(E element) {
+    /**
+     * insert new node into tail list
+     *
+     * @param data specific element for insert into node
+     */
+    private void linkLast(E data) {
         Node<E> current = head;
         while (current.next != null) {
             current = current.next;
         }
-        current.setNext(new Node<>(element));
+        current.setNext(new Node<>(data));
     }
 
     /**
-     * Заменяет элемент в указанной позиции в этом списке указанным элементом.
-     * Вызывает IndexOutOfBoundsException,
-     * если указанный индекс выходит за пределы диапазона (index < 0 || index >= size()).
+     * Replaces the element at the specified position in this list with the specified element.
+     * Throws IndexOutOfBoundsException,
+     * if the specified index is out of range (index < 0 || index >= size()).
      */
-    public boolean set(int index, E element) {
+    public boolean set(int index, E data) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Wrong index " + index);
         }
-        if (head.data.equals(element) && index == 0) {
+        if (head.data.equals(data) && index == 0) {
             return true;
         } else {
             Node<E> temp = head;
             int k = 0;
             while (temp.getNext() != null || k == index) {
                 if (k == index) {
-                    temp.data = element;
+                    temp.data = data;
                     return true;
                 }
                 temp = temp.getNext();
@@ -165,7 +216,12 @@ public class ShppLinkedList<E> implements Iterable<E> {
         return false;
     }
 
-
+    /**
+     * returns the element in the given index
+     *
+     * @param index number node
+     * @return data node
+     */
     public E get(int index) {
         if (isIndexNoCorrect(index)) {
             throw new IndexOutOfBoundsException("wrong index " + index);
@@ -184,12 +240,18 @@ public class ShppLinkedList<E> implements Iterable<E> {
         return null;
     }
 
+    /**
+     * check if index possible
+     *
+     * @param index current number for check
+     * @return true if index possible
+     */
     private boolean isIndexNoCorrect(int index) {
         return (index < 0 || index >= size);
     }
 
     /**
-     * remove first
+     * remove first node
      */
     public E remove() {
         Node<E> temp = head;
@@ -198,13 +260,18 @@ public class ShppLinkedList<E> implements Iterable<E> {
         return temp.data;
     }
 
+    /**
+     * get size linkedList
+     *
+     * @return number nodes
+     */
     public int size() {
         return size;
     }
 
     /**
-     * Удаляет элемент в указанной позиции в этом списке.
-     * Вызывает исключение NoSuchElementException, если этот список пуст.
+     * Removes the element at the specified position in this list.
+     * Throws NoSuchElementException if this list is empty.
      */
     public E remove(int index) {
         if (isIndexNoCorrect(index)) {
@@ -252,11 +319,16 @@ public class ShppLinkedList<E> implements Iterable<E> {
         return pred;
     }
 
-    public int indexOf(E e) {
+    /**
+     * return number node with first equals with data
+     * @param data specific element
+     * @return number position if found
+     */
+    public int indexOf(E data) {
         Node<E> current = head;
         int k = 0;
         while (current != null) {
-            if (current.data.equals(e)) {
+            if (current.data.equals(data)) {
                 return k;
             }
             current = current.next;
@@ -265,6 +337,10 @@ public class ShppLinkedList<E> implements Iterable<E> {
         return -1;
     }
 
+    /**
+     * convert linkedList to array
+     * @return array
+     */
     public Object[] toArray() {
         if (head == null) {
             return null;
@@ -293,6 +369,9 @@ public class ShppLinkedList<E> implements Iterable<E> {
         }
     }
 
+    /**
+     * clear all nodes
+     */
     public void clear() {
         if (head != null) {
             Node<E> next = head.next;
@@ -308,6 +387,10 @@ public class ShppLinkedList<E> implements Iterable<E> {
         }
     }
 
+    /**
+     * representation in the form of a String
+     * @return string include all nodes
+     */
     @Override
     public String toString() {
         if (size == 0) {
@@ -323,13 +406,18 @@ public class ShppLinkedList<E> implements Iterable<E> {
         return joiner.toString();
     }
 
-    public boolean contains(E element) {
+    /**
+     * find specific element into list
+     * @param data specific element
+     * @return true if present
+     */
+    public boolean contains(E data) {
         if (head == null) {
             return false;
         } else {
             Node<E> current = head;
             while (current != null) {
-                if ((current.data == null && element == null) || current.data.equals(element)) {
+                if ((current.data == null && data == null) || current.data.equals(data)) {
                     return true;
                 }
                 current = current.next;
@@ -338,13 +426,20 @@ public class ShppLinkedList<E> implements Iterable<E> {
         return false;
     }
 
+    /**
+     * it's return implementation fail fast iterator
+     * @return iterator
+     */
     @Override
     public Iterator<E> iterator() {
         return new InnerListIterator();
     }
 
-
-    class Node<E> {
+    /**
+     * inner class node
+     * @param <E> specific data
+     */
+    private static class Node<E> {
         private E data;
         private Node<E> next;
 
